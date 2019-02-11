de4dfd94ef8d032ca97d56da66db0f07e75cea6e
Ryan Rottmann <ryanrottmann@Ryans-MacBook-Pro.local>
2019-02-11 15:48:45 -0600

    first commit

---
+++ Dog/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    enum Gender
+    {
+        Male, 
+        Female
+    }
+    class Dog
+    {
+        public String name;
+        public String owner;
+        public int age;
+        public Gender gender;
+        public Dog (string name, String owner, int age, Gender gender)
+        {
+
+
+        }
+    }
+}
