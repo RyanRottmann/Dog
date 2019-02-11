ee1ed34a83d49260c425bcbd25957cc7a5966f2c
Ryan Rottmann <ryanrottmann@Ryans-MacBook-Pro.local>
2019-02-11 17:03:30 -0600

    second commit

--- Dog/Program.cs
+++ Dog/Program.cs
@@ -6,7 +6,20 @@ namespace Dog
     {
         static void Main(string[] args)
         {
+            Dog dog = new Dog("Name1","Owner1",4, Gender.Male);
+            int numOfBarks;
+            Console.WriteLine("How many barks?");
+            numOfBarks = int.Parse(Console.ReadLine());
+            dog.Bark(numOfBarks);
+            Console.WriteLine(dog.GetTag());
 
+            Dog puppy = new Dog("Orion", "Shawn", 1, Gender.Male);
+            puppy.Bark(3);
+            Console.WriteLine(puppy.GetTag());
+
+            Dog myDog = new Dog("Lileu", "Dale", 4, Gender.Female);
+            myDog.Bark(1);
+            Console.WriteLine(myDog.GetTag());
         }
     }
     enum Gender
@@ -22,8 +35,45 @@ namespace Dog
         public Gender gender;
         public Dog (string name, String owner, int age, Gender gender)
         {
-
-
+            this.name = name;
+            this.owner = owner;
+            this.age = age;
+            this.gender = gender;
+        }
+        public void Bark (int num)
+        {
+            while(num > 0)
+            {
+                Console.WriteLine("Bark");
+                num--;
+            }
+        }
+        public String GetTag()
+        {
+            String returnString;
+            if(gender == Gender.Male)
+            {
+                if (age == 1)
+                {
+                    returnString = "If lost, call " + owner + ". His name is " + name + " and he is " + age + " year old.";
+                }
+                else
+                {
+                    returnString = "If lost, call " + owner + ". His name is " + name + " and he is " + age + " years old.";
+                }
+            }
+            else
+            {
+                if (age == 1)
+                {
+                    returnString = "If lost, call " + owner + ". Her name is " + name + " and she is " + age + " year old.";
+                }
+                else
+                {
+                    returnString = "If lost, call " + owner + ". Her name is " + name + " and she is " + age + " years old.";
+                }
+            }
+            return returnString;
         }
     }
 }
